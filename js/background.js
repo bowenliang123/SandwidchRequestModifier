576e1596ac981f305ad79ba9013bf7bc2ff0ca43
梁博文 <bowen.liang.123@gmail.com>
2016-03-31 22:13:57 +0800

    改为从背景页获取数据

--- js/background.js
+++ js/background.js
@@ -17,7 +17,8 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 //https://developer.chrome.com/extensions/webRequest#event-onBeforeRequest
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
-        console.log(details);
+        //console.log(details);
+
         //检查 URL 参数并加入
         //if (details.url.indexOf('#34567') < 0) {
         //    return {redirectUrl: details.url + '#34567'}
@@ -39,10 +40,48 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
     {urls: ["<all_urls>"]},
     ["blocking", "requestHeaders"]);
 
+//监听请求
+chrome.extension.onRequest.addListener(
+    function (request, sender, sendResponse) {
+        if (request.action == "getAllCases")
+            {
+                var allCases = getAllCases();
+                console.log(allCases);
+                sendResponse({cases: allCases});
+            }
+        else
+            sendResponse({}); // snub them.
+    });
 
 function addHeaders(requestHeaders, additionalHeaders) {
     let keys = Object.keys(additionalHeaders);
     keys.forEach((key)=> {
         requestHeaders.push({name: key, value: additionalHeaders[key]});
     })
+}
+
+function getAllCases() {
+    return [
+        {
+            caseId: 0,
+            name: 'UC',
+            ua: 'Mozilla/5.0 (Linux; U; Android 4.4.4; zh-CN; MI 4LTE Build/KTU84P) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.9.2.712 U3/0.8.0 Mobile',
+            headers: "headerKey:headerValue0",
+            params: "paramKey=paramValue0"
+        },
+        {
+            caseId: 1,
+            name: 'Wechat',
+            ua: 'mozilla/5.0 (linux; u; android 4.1.2; zh-cn; mi-one plus build/jzo54k) applewebkit/534.30 (khtml, like gecko) version/4.0 mobile safari/534.30 micromessenger/5.0.1.352                ',
+            headers: "headerKey:headerValue1",
+            params: "paramKey=paramValue1"
+        },
+        {
+            caseId: 2,
+            name: 'Weibo',
+            ua: 'Mozilla/5.0 (Linux; U; Android 4.0.4; zh-cn; HTC Sensation XE with Beats Audio Z715e Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+            headers: "headerKey:headerValue2",
+            params: "paramKey=paramValue2"
+        }
+    ];
 }
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -5,37 +5,26 @@
 
 angular.module('mainCtrl', [])
     .controller('mainCtrl', ($scope) => {
-        $scope.cases = [
-            {
-                caseId: 0,
-                name: 'UC',
-                ua: 'Mozilla/5.0 (Linux; U; Android 4.4.4; zh-CN; MI 4LTE Build/KTU84P) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/10.9.2.712 U3/0.8.0 Mobile',
-                headers: "headerKey:headerValue0",
-                params: "paramKey=paramValue0"
-            },
-            {
-                caseId: 1,
-                name: 'Wechat',
-                ua: 'mozilla/5.0 (linux; u; android 4.1.2; zh-cn; mi-one plus build/jzo54k) applewebkit/534.30 (khtml, like gecko) version/4.0 mobile safari/534.30 micromessenger/5.0.1.352                ',
-                headers: "headerKey:headerValue1",
-                params: "paramKey=paramValue1"
-            },
-            {
-                caseId: 2,
-                name: 'Weibo',
-                ua: 'Mozilla/5.0 (Linux; U; Android 4.0.4; zh-cn; HTC Sensation XE with Beats Audio Z715e Build/IML74K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
-                headers: "headerKey:headerValue2",
-                params: "paramKey=paramValue2"
-            }
-        ];
+        $scope.cases = [];
 
         $scope.isActive = false;
         $scope.showCase = $scope.cases[0];
         $scope.activeCase = $scope.cases[0];
 
+        init();
+
+        function init (){
+            chrome.extension.sendRequest({action: "getAllCases"}, (response) => {
+                console.log(response);
+                $scope.cases = response.cases;
+
+                //force refresh
+                $scope.$apply();
+            });
+        };
+
         $scope.changeCaseId = function (c) {
             $scope.showCase = c;
-            console.log($scope.showCase);
         };
 
         $scope.activate = function (c) {

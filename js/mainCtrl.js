12b3d53311d0e58ba93462f691cf25328c0621ff
梁博文 <lbw105516@alibaba-inc.com>
2016-04-02 23:27:39 +0800

    实现导入导出的功能

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -10,36 +10,29 @@ angular.module('mainCtrl', [])
         //初始化
         init();
 
-        /**
-         * 初始化
-         */
-        function init() {
-
-            $scope.cases = [];
-            $scope.showCase = undefined;
-            $scope.activeCase = undefined;
-
-            //getAllCases
+        function getAllCases() {
+//getAllCases
             chrome.extension.sendRequest({action: "getAllCases"}, (response) => {
-                console.log(response);
-                var cases = response.cases;
+                let cases = response.cases;
                 if (cases && cases.length > 0) {
                     $scope.cases = cases;
 
                     if (!$scope.showCase) {
                         $scope.showCase = $scope.cases[0];
 
-                        $scope.parseAndUpdateUaInfo($scope.showCase.ua);
+                        $scope.parseAndUpdateUaInfo();
                     }
 
                     //force refresh
                     $scope.$apply();
                 }
             });
+        }
 
-            //getActiveCase
+        //getActiveCase
+        function getActiveCase() {
+
             chrome.extension.sendRequest({action: "getActiveCase"}, (response) => {
-                console.log(response);
                 let activeCase = response.activeCase;
                 if (!activeCase) {
                     $scope.activeCase = undefined;
@@ -49,15 +42,63 @@ angular.module('mainCtrl', [])
                 $scope.activeCase = activeCase;
                 $scope.showCase = activeCase;
 
-                $scope.parseAndUpdateUaInfo($scope.showCase.ua);
+                $scope.parseAndUpdateUaInfo();
 
                 //force refresh
                 $scope.$apply();
 
             });
-
-
         }
+
+        /**
+         * 触发下载二维码文件
+         * @param base64img
+         */
+        function invokeDownload(content) {
+            //初始化链接
+            let downloadLink = document.createElement("a");
+
+            //用 base64 生成 url
+            downloadLink.href = 'data:text/plain,' + content;
+
+            //文件名
+            downloadLink.download = 'export-' + new Date().getTime() + '.json';
+
+            //加入到文档中
+            document.body.appendChild(downloadLink);
+
+            //下载
+            downloadLink.click();
+
+            //清理节点
+            document.body.removeChild(downloadLink);
+        }
+
+        /**
+         * 初始化
+         */
+        function init() {
+
+            $scope.cases = [];
+            $scope.showCase = undefined;
+            $scope.activeCase = undefined;
+
+            getAllCases();
+
+            getActiveCase();
+
+            addInputLIstener();
+        }
+
+        //导入所有用例
+        $scope.importCases = ()=> {
+            $('#uploadInput')[0].click();
+        };
+
+        //导出所有用例
+        $scope.exportCases = ()=> {
+            invokeDownload(angular.toJson($scope.cases));
+        };
 
         $scope.changeShowCase = (simCase)=> {
             $scope.showCase = simCase;
@@ -72,7 +113,7 @@ angular.module('mainCtrl', [])
             })
         };
 
-        $scope.deActivateCase = ()=> {
+        $scope.deactivateCase = ()=> {
             //update local
             $scope.activeCase = undefined;
 
@@ -97,7 +138,7 @@ angular.module('mainCtrl', [])
             }, []);
 
             //持久化所有用例
-            persistAllCases($scope.cases);
+            $scope.persistAllCases($scope.cases);
         };
 
         $scope.saveCase = (simCaseId)=> {
@@ -129,12 +170,13 @@ angular.module('mainCtrl', [])
 
             //持久化所有用例
             persistAllCases($scope.cases);
-        }
+        };
 
         //local methods
 
         //解析并更新 UA 信息
-        $scope.parseAndUpdateUaInfo = (uaStr) => {
+        $scope.parseAndUpdateUaInfo = () => {
+            let uaStr = $scope.showCase.ua;
             if (!uaStr) {
                 return;
             }
@@ -143,18 +185,52 @@ angular.module('mainCtrl', [])
             uaParser.setUA(uaStr);
 
             $scope.uaInfo = uaParser.getResult();
-            console.log($scope.uaInfo);
+        };
+
+        //持久化所有用例数据到后台
+        $scope.persistAllCases = (simCases) => {
+            chrome.extension.sendRequest({
+                action: "saveAllCases",
+                casesStr: angular.toJson(simCases)  //序列化
+            }, (response) => {
+            });
+        };
+
+        //响应JSON上传input 元素点击事件
+        function addInputLIstener() {
+            $('#uploadInput').change((e) => {
+                console.log(e);
+                let filePath = $('#uploadInput')[0].files[0];
+                let reader = new FileReader();
+
+                if (filePath) {
+                    //响应事件 -  文件读取完成
+                    reader.addEventListener("load", ()=> {
+                        let fileContent = reader.result;
+                        //parse json and update cases
+                        $scope.cases = JSON.parse(fileContent);
+
+                        //persist data
+                        $scope.persistAllCases($scope.cases);
+
+                        //deactivate all cases
+                        $scope.deactivateCase();
+
+                        //force refresh
+                        $scope.$apply();
+                    }, false);
+
+
+                    //将文件读取为 Base64格式
+                    reader.readAsText(filePath);
+                }
+            });
         }
+
+        addInputLIstener();
 
     })
 ;
 
-//持久化所有用例数据到后台
-function persistAllCases(simCases) {
-    chrome.extension.sendRequest({
-        action: "saveAllCases",
-        casesStr: angular.toJson(simCases)  //序列化
-    }, (response) => {
-    });
-}
+
 
